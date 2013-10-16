71298674cafb1bb19517ae73d7b76031b40b0631
Denys Lózko <denis.lozko@gmail.com>
2013-10-16 13:56:19 +0300

    version update for testing

--- jiraFixes.user.js
+++ jiraFixes.user.js
@@ -4,7 +4,7 @@
 // @description    Some minor fixes for JIRA
 // @include        http://jira.odesk.com/*
 // @updateURL      https://gist.github.com/talmuth/e3abd629add49c0afd4f/raw/jiraFixes.user.js
-// @version        0.2.1
+// @version        0.2.2
 // @require        https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
