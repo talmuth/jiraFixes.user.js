d4ff59bf4ac46fbe6a8862b72011163ab92f175e
Denys Lózko <denis.lozko@gmail.com>
2013-10-31 11:31:48 +0200

    make updateurl fancy

--- jiraFixes.user.js
+++ jiraFixes.user.js
@@ -3,8 +3,8 @@
 // @namespace      https://gist.github.com/talmuth/e3abd629add49c0afd4f
 // @description    Some minor fixes for JIRA
 // @include        http://jira.odesk.com/*
-// @updateURL      http://bit.ly/1h2yMDP
-// @version        0.6.1
+// @updateURL      http://bit.ly/ag-jira-js-tweaks
+// @version        0.6.2
 // @require        https://gist.github.com/BrockA/2625891/raw/waitForKeyElements.js
 // @require        http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js
 // ==/UserScript==
