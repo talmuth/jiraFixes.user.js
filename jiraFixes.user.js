bfef9fbb095d246c1776380c1681ac3caafc2eb1
Denys Lozko <code@cl4p-tp.it>
2019-10-29 18:56:24 -0700

    Fix issues without epic link

--- jiraFixes.user.js
+++ jiraFixes.user.js
@@ -4,7 +4,7 @@
 // @description    Some minor fixes for JIRA
 // @include        https://jira.odesk.com/*
 // @updateURL      http://bit.ly/bpa-ag-jira-js-tweaks-v2
-// @version        0.15.3
+// @version        0.15.4
 // @resource       UI_CSS http://bit.ly/bpa-ag-jira-css-for-usersript
 // @require        https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js
 // @grant          GM_addStyle
